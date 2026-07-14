876da33918f675bca419fea29854ab766236fc63
numan6t9 <al.32noman@gmail.com>
2026-07-14 23:20:11 +0600

    ....

--- src/main/java/librarymanagement/librarymanagement/LogInViewController.java
+++ src/main/java/librarymanagement/librarymanagement/LogInViewController.java
@@ -1,7 +1,26 @@
 package librarymanagement.librarymanagement;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LogInViewController
 {
     @javafx.fxml.FXML
+    private TextField enterUserId;
+    @javafx.fxml.FXML
+    private PasswordField enterPassword;
+    @javafx.fxml.FXML
+    private ComboBox<String> enterUserType;
+    @javafx.fxml.FXML
+    private TextField enterUserName;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void LogInButton(ActionEvent actionEvent) {
+    }
+}
